537b5654e353635477fd2eb06911a611271e8605
blake-brooks-88 <blakerbrooksbrb@gmail.com>
2025-04-05 13:05:16 -0500

    create application user model

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MrPanchoRestaurant.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ICollection<Order>? Orders { get; set; }
+
+    }
+}
