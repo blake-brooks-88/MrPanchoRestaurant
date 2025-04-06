b4df2c3f05c75cf084e4b89df5848c76e10d5a84
blake-brooks-88 <blakerbrooksbrb@gmail.com>
2025-04-05 19:39:37 -0500

    create add/update controller methods

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -7,16 +7,34 @@ namespace MrPanchoRestaurant.Controllers
     public class ProductController : Controller
     {
         private Repository<Product> products;
+        private Repository<Ingredient> ingredients;
 
         public ProductController(ApplicationDbContext context)
         {
             products = new Repository<Product>(context);
+            ingredients = new Repository<Ingredient>(context); ;
         }
 
         public async Task<IActionResult> Index()
         {
             return View(await products.GetAllAsync());
 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddEdit(int id)
+        {
+            ViewBag.Ingredients = await ingredients.GetAllAsync();
+            if (id == 0)
+            {
+                ViewBag.Operation = "Add";
+                return View(new Product());
+            }
+            else
+            {
+                ViewBag.Operation = "Edit";
+                return View();
+            }
         }
     }
 }
