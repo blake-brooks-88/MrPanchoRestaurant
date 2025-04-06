3459b27f817f31ff065972c6cb7813315cdde967
blake-brooks-88 <blakerbrooksbrb@gmail.com>
2025-04-06 14:45:43 -0500

    allow user to add items to order

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MrPanchoRestaurant.Data;
+using MrPanchoRestaurant.Models;
+
+namespace MrPanchoRestaurant.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private Repository<Product> _products;
+        private Repository<Order> _orders;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public OrderController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _products = new Repository<Product>(context);
+            _orders = new Repository<Order>(context);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var model = HttpContext.Session.Get<OrderViewModel>("OrderViewModel") ?? new OrderViewModel
+            {
+                OrderItems = new List<OrderItemViewModel>(),
+                Products = await _products.GetAllAsync()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [Authorize]
+
+        public async Task<IActionResult> AddItem(int prodId, int prodQty)
+        {
+            var product = await _context.Products.FindAsync(prodId);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            var model = HttpContext.Session.Get<OrderViewModel>("OrderViewModel") ?? new OrderViewModel
+            {
+                OrderItems = new List<OrderItemViewModel>(),
+                Products = await _products.GetAllAsync()
+            };
+
+            var existingItem = model.OrderItems.FirstOrDefault(oi => oi.ProductId == prodId);
+
+            if (existingItem != null)
+            {
+                existingItem.Quantity += prodQty;
+            }
+            else
+            {
+                model.OrderItems.Add(new OrderItemViewModel
+                {
+                    ProductId = product.ProductId,
+                    Price = product.Price,
+                    Quantity = prodQty,
+                    ProductName = product.Name
+                });
+            }
+
+            model.TotalAmount = model.OrderItems.Sum(oi => oi.Price * oi.Quantity);
+
+            HttpContext.Session.Set("OrderViewModel", model);
+
+            return RedirectToAction("Create", model);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
